f7dba8c0ba407bd008b9ac4f69c5f9286f0005e8
Valermos <Valermos@posteo.org>
2017-03-18 07:38:39 +0100

    version share update (v.1.6.7.1)
    Merged mining is now active

--- html/ajax/get_versionshare.php
+++ html/ajax/get_versionshare.php
@@ -44,30 +44,15 @@ if (!empty($_COOKIE["lang"])) {
 	{
 		$count=bcmul(bcdiv($row['count'],1000,4),100,2);
 		$version=$row['version'];
-		if ($version > 4 && 0 === strpos($version, '4')) {
+		if ($version == 43646981) {
 			$version="5 (since client v.0.6.0)";
-			$barcount=$count;
+			$barcount+=$count;
+		}
+		if ($version == 43647237) {
+			$version="5 (Merged Mining)";
+			$barcount+=$count;
 		}
 		echo '<tr><td>'.$version.'</td><td>'.$count.' %</td></tr>';
 	}
 		?>
-	<tr><td colspan=2><div class="progress">
-	  <div class="progress-bar" role="progressbar" aria-valuenow="<?php echo $barcount; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $barcount; ?>%;">
-	    <?php
-				echo $barcount.' %';
-			?>
-	  </div>
-		<?php
-			$barLeft=bcsub(95,$barcount,2);
-			if ($barLeft<0) {
-				$barLeft=0;
-			}
-		?>
-		<div class="progress-bar progress-bar-warning progress-bar-striped" role="progressbar" aria-valuenow="<?php echo $barLeft; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $barLeft; ?>%;">
-	    <?php
-				echo $barLeft.' % left';
-			?>
-	  </div>
-	</div>
-	Merged mining starts if version 5 has 95%</td></tr>
 	</tbody></table>
